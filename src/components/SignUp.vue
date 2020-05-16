e76c4c2c195b76b1f3b7030a001cbf2d2e5cfdb5
timer-coder <QJ15827054817@163.com>
2020-05-16 22:32:49 +0800

    first commit

---
+++ src/components/SignUp.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="background">
+        <pre>    请用户遵守以下协议：</pre>
+        <pre></pre>
+        <div class="signup-box">
+            <div>
+                <img src="../assets/Logo2.png" style="width:150px; height:150px">
+            </div>
+            <el-form label-width="80px" class="signup-form" size="mini">
+                <el-form-item style="font-family: 宋体">
+                    <el-radio v-model="radio" label="1">买家账号</el-radio>
+                    <el-radio v-model="radio" label="2">卖家账号</el-radio>
+                </el-form-item>
+                <el-form-item label="昵称" style="font-family: 宋体">
+                    <el-input style="width:240px"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号" style="font-family: 宋体">
+                    <el-input style="width:240px"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" style="font-family: 宋体">
+                    <el-input style="width:240px"></el-input>
+                </el-form-item>
+                <el-form-item label="确定密码" style="font-family: 宋体">
+                    <el-input style="width:240px"></el-input>
+                </el-form-item>
+                <el-form-item class="button-box">
+                    <el-button style="background: #b0dddf">注册</el-button>
+                    <el-button style="background: #b0dddf">取消</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      radio: '1'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+html,
+body,
+#app,
+.background {
+    background-color: #b0dddf;
+    max-width: 100%;
+    max-height: 100%;
+    .signup-box {
+        width: 350px;
+        height: 500px;
+        background-color: #fff;
+        border-radius: 3px;
+        position: absolute;
+        left: 70%;
+        top: 40px;
+        img {
+            position: absolute;
+            left:30%;
+        }
+    }
+}
+.signup-form {
+    position: absolute;
+    bottom: 15%;
+    .button-box {
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home.vue'
+import HomeLogin from '../components/HomeLogin.vue'
+import SignUp from '../components/SignUp.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: Home },
+  { path: '/homelogin', component: HomeLogin },
+  { path: '/signup', component: SignUp }
 ]
 
 const router = new VueRouter({
