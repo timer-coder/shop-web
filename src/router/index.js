5fa01b6296a389b8b73e2e8fec8f93670e444eba
timer-coder <QJ15827054817@163.com>
2020-06-03 23:30:59 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import Home from '../components/Home.vue'
 import HomeLogin from '../components/HomeLogin.vue'
 import SignUp from '../components/SignUp.vue'
 import UserHome from '../components/UserHome.vue'
+import HaveBuy from '../components/HaveBuy.vue'
+import HaveSell from '../components/HaveSell.vue'
+import Like from '../components/Like.vue'
+import SellingGoods from '../components/SellingGoods.vue'
+import ShoppingCart from '../components/ShoppingCart.vue'
+import BoyShirt from '../goods/clothes/BoyShirt.vue'
+import Earphone from '../goods/phone/Earphone.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +19,14 @@ const routes = [
   { path: '/home', component: Home },
   { path: '/homelogin', component: HomeLogin },
   { path: '/signup', component: SignUp },
-  { path: '/userhome', component: UserHome }
+  { path: '/userhome', component: UserHome },
+  { path: '/havebuy', component: HaveBuy },
+  { path: '/havesell', component: HaveSell },
+  { path: '/like', component: Like },
+  { path: '/sellinggoods', component: SellingGoods },
+  { path: '/shoppingcart', component: ShoppingCart },
+  { path: '/boyshirt', component: BoyShirt },
+  { path: '/earphone', component: Earphone }
 ]
 
 const router = new VueRouter({
