ab770ab751e25190684dc71b9987d460df3e2298
timer-coder <QJ15827054817@163.com>
2020-06-12 17:57:02 +0800

    first commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,97 @@
 <template>
-    <div></div>
+          <el-container>
+        <!--顶端操作栏-->
+        <el-header class="top-header">
+            <Head></Head>
+        </el-header>
+        <el-container>
+            <!--侧边栏导航-->
+            <el-aside width="200px" style="background-color: #545c64" class="left-aside">
+                <Left></Left>
+            </el-aside>
+            <el-container>
+                <el-main class="top-main">
+                    <!--广告栏-->
+                    <el-header class="medium-header" height="230px">
+                        <Ad></Ad>
+                    </el-header>
+                  <!--猜你喜欢-->
+                    <el-header class="bottom-header">
+                        <span>猜你喜欢<i class="el-icon-arrow-right"></i></span>
+                    </el-header>
+                  <!--推荐-->
+                    <el-main>
+                        <Recommend></Recommend>
+                        <el-header></el-header>
+                        <Recommend></Recommend>
+                    </el-main>
+                </el-main>
+            </el-container>
+        </el-container>
+             <el-main>
+            <el-form label-width="80px" class="user-form" size="mini">
+                            <el-form-item label="username" style="font-family: 宋体">
+                                <el-input v-model="username" style="width:240px"></el-input>
+                            </el-form-item>
+                            <el-form-item label="password" style="font-family: 宋体">
+                                <el-input v-model="password" style="width:240px"></el-input>
+                            </el-form-item>
+                            <el-form-item class="button-box">
+                                <el-button style="background: #b0dddf" v-on:click="login()">登陆</el-button>
+                                <el-button style="background: #b0dddf">找回密码</el-button>
+                            </el-form-item>
+            </el-form>
+      </el-main>
+          </el-container>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseUrl } from '../js/config'
+import Head from './Head'
+import Left from './Left'
+import Ad from './Ad'
+
 export default {
+  data () {
+    return {
+      username: null,
+      password: null
+    }
+  },
+  components: {
+    Head,
+    Left,
+    Ad
+  },
+  methods: {
+    login: function () {
+      const that = this
+      console.log(that.username)
+      console.log(that.password)
+      const formData = new FormData()
+      formData.append('username', Number(this.username))
+      formData.append('password', Number(this.password))
+      axios.create({
+        headers: { 'Content-Type': 'application/json' }
+      }).post(baseUrl + '/login', require('qs').stringify({
+        username: Number(this.username),
+        password: Number(this.password)
+      }), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(response => {
+        if (response.data === '登陆成功') {
+          this.$router.push({ path: 'homelogin' })
+        } else {
+          alert('账号或者密码错误')
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>
 
